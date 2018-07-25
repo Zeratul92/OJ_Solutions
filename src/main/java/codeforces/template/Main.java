029b7dd8dc695f17bab2f8f3017f89b813600941
Zeratul92 <weikaizeng@126.com>
2018-07-26 01:39:29 +0800

    add some template files

---
+++ src/main/java/codeforces/template/Main.java
@@ -0,0 +1,18 @@
+package codeforces.template;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * A code template for codeforces java(1.8) submit
+ * Use Scanner to get standard input.
+ * The top line 'package ...' needs to be deleted before submit.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        while (in.hasNext()) {
+            System.out.println("hello");
+        }
+    }
+}
---
+++ src/main/java/leetcode_cn/template/Solution.java
@@ -0,0 +1,17 @@
+package leetcode_cn.template;
+
+/**
+ * A general code template class for leetcode java submit.
+ * Use main function to debug and test.
+ * The top line 'package ...' and 'main' function need to be deleted before submit.
+ */
+public class Solution {
+
+    public void solve() {
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solve();
+    }
+}
