c240907c29b7495a3bc00aad4fbf6baf53463e7e
Zeratul92 <weikaizeng@126.com>
2018-07-26 01:42:43 +0800

    solve codeforces problem http://codeforces.com/problemset/problem/1/A

---
+++ src/main/java/codeforces/problem_1a/Main.java
@@ -0,0 +1,23 @@
+package codeforces.problem_1a;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * <a href="http://codeforces.com/problemset/problem/1/A">Theatre Square</a>
+ * Created by Zeratul on 2018/7/25.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        while (in.hasNext()) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int a = in.nextInt();
+            long x = n / a + (n % a != 0 ? 1 : 0);
+            long y = m / a + (m % a != 0 ? 1 : 0);
+            System.out.println(x * y);
+        }
+    }
+}
