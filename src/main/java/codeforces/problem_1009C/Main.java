9e9f3711154e923dd56fc3ece212a58dcd1237f4
Zeratul92 <weikaizeng@126.com>
2018-07-26 20:23:34 +0800

    solve http://codeforces.com/problemset/problem/1009/C

--- src/main/java/codeforces/problem_1009C/Main.java
+++ src/main/java/codeforces/problem_1009C/Main.java
@@ -1,7 +1,33 @@
 package codeforces.problem_1009C;
 
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
 /**
+ * <a href='http://codeforces.com/problemset/problem/1009/C'>Annoying Present</a>
+ * Mind the precision issue, better use long type to calculate the sum.
  * Created by Zeratul on 2018/7/26.
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+        while (in.hasNext()) {
+            long n = in.nextInt();
+            int m = in.nextInt();
+            long minDist = (n * n) / 4;
+            long maxDist = n * (n - 1) / 2;
+//            System.out.println(minDist + ", " + maxDist);
+            long sum = 0;
+            for (int i = 0; i < m; i++) {
+                long x = in.nextInt();
+                long d = in.nextInt();
+                if (d >= 0.0) {
+                    sum += x * n + d * maxDist;
+                } else {
+                    sum += x * n + d * minDist;
+                }
+            }
+            System.out.println(String.format("%.7f", (double) sum / (double) n));
+        }
+    }
 }
