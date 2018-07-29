ea09636794419d4ffd849fdece68226979967201
Zeratul92 <weikaizeng@126.com>
2018-07-30 06:54:15 +0800

    modify codeforces template

--- src/main/java/codeforces/CodeforcesTemplate.java
+++ src/main/java/codeforces/CodeforcesTemplate.java
@@ -4,11 +4,12 @@ import java.io.BufferedInputStream;
 import java.util.Scanner;
 
 /**
- * A code template for codeforces java(1.8) submit
+ * A template for codeforces java(1.8) submit.
  * Use Scanner to get standard input.
- * The top line 'package ...' needs to be deleted before submit.
+ * Use System.out to output.
+ * The top line 'package ...' needs to be neglected before submit.
  */
-public class Problem {
+public class CodeforcesTemplate {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedInputStream(System.in));
         while (in.hasNext()) {
