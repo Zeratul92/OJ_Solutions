d2b3651393ddfc69251095c549750d70c06b327b
Zeratul92 <weikaizeng@126.com>
2018-09-01 20:58:57 +0800

    solve

---
+++ src/main/java/leetcode/Algorithm_49_Group_Anagrams.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Algorithm_49_Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        Algorithm_49_Group_Anagrams s = new Algorithm_49_Group_Anagrams();
+        System.out.println(s.groupAnagrams(new String[]{"abc", "bac", "abd"}));
+    }
+}
