8383711de097e9f56a12521b80f9abee55aa0f0d
Zeratul92 <weikaizeng@126.com>
2019-01-29 23:02:33 +0800

    add reverse pairs counting method in Array.java

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,4 +1,53 @@
 public class Array {
+
+    private static int getReversePairCnt(int[] a) {
+        if (a == null || a.length == 0) {
+            return 0;
+        }
+        int[] copy = new int[a.length];
+        int[] backup = new int[a.length];
+        System.arraycopy(a, 0, copy, 0, a.length);
+        System.arraycopy(a, 0, backup, 0, a.length);
+        int ret = mergeCount(a, copy, 0, a.length - 1);
+        System.arraycopy(backup, 0, a, 0, a.length);
+        printArray(a);
+        return ret;
+    }
+
+    private static int mergeCount(int[] a, int[] copy, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int mid = (l + r) >> 1;
+        int lCnt = mergeCount(copy, a, l, mid);
+        int rCnt = mergeCount(copy, a, mid + 1, r);
+
+        int i = mid, j = r;
+        int pos = r;
+        int cnt = 0;
+        while (i >= l && j >= mid + 1) {
+            if (a[i] > a[j]) {
+                copy[pos--] = a[i--];
+                cnt += j - mid;
+            } else {
+                copy[pos--] = a[j--];
+            }
+        }
+        while (i >= l) {
+            copy[pos--] = a[i--];
+        }
+        while (j >= mid + 1) {
+            copy[pos--] = a[j--];
+        }
+//        System.out.println("begin: " + l + ", r: " + r + ", cnt = " + cnt + ", left: " + lCnt + ", right = " + rCnt);
+//        System.out.print("a: " + a.toString() + " ");
+//        printArray(a);
+//        System.out.print("c: " + copy.toString() + " ");
+//        printArray(copy);
+        return cnt + lCnt + rCnt;
+    }
+
+
     private static void printArray(int[] a) {
         StringBuilder sb = new StringBuilder();
         sb.append('[');
@@ -23,6 +72,7 @@ public class Array {
      * <a href='https://www.cnblogs.com/mydesky2012/p/5620006.html'>Reference</a>
      * Get next permutation of the array, which rearranges numbers into the lexicographically next greater permutation of numbers.
      * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+     *
      * @param nums the array
      */
     private static void nextPermutation(int[] nums) {
@@ -56,12 +106,24 @@ public class Array {
         }
     }
 
+    private static int[] str2Array(String str) {
+        int[] ret = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ret[i] = str.charAt(i) - '0';
+        }
+        return ret;
+    }
+
 
     public static void main(String[] args) {
         int[] a = {2, 4, 3, 1};
         printArray(a);
         nextPermutation(a);
         printArray(a);
+
+
+        String str = "315462";
+        System.out.println(getReversePairCnt(str2Array(str)));
 
     }
 
